1037aa88c3b9674d4c58187472656fb1f7e13135
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-05-24 22:44:39 +0300

    Unohtunut asia ja poista duplikaatit

--- match.py
+++ match.py
@@ -1,10 +1,19 @@
+#Käsittele osittain parsatut
 rivit = open('urls.txt', 'r')
 rivit = rivit.readlines()
- 
+
+valmiitirvit = []
+
 count = 0
-# Strips the newline character
 for line in rivit:
     count += 1
     if line.strip().startswith("Location: "):
         if not "<div " in line.strip():
             print(line.strip().replace("Location: ", ""))
+            valmiitirvit.append(line.strip().replace("Location: ", "") + "\n")
+
+
+#Tallenna valmiit
+f = open("finalurls.txt", "w")
+f.write(''.join([str(x) for x in list(dict.fromkeys(valmiitirvit))]))
+f.close()
